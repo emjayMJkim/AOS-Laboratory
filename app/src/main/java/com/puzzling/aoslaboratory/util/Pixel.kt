d5173d0ee0af991bf867098542a6c5986f48dbdc
Cat-JiYoung <kimyamae82@gmail.com>
2023-06-30 15:09:04 +0900

    [CHORE] CI 테스트

---
+++ app/src/main/java/com/puzzling/aoslaboratory/util/Pixel.kt
@@ -0,0 +1,9 @@
+package com.puzzling.aoslaboratory.util
+
+import android.content.res.Resources
+
+val Int.dp
+    get() = this * Resources.getSystem().displayMetrics.density.toInt()
+
+val Int.px
+    get() = (this * Resources.getSystem().displayMetrics.density).toInt()
