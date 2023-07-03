651379ac24ac31456aec759016f0c59674d132f7
Cat-JiYoung <kimyamae82@gmail.com>
2023-07-03 19:21:05 +0900

    [FEAT] 등록하기 버튼 클릭시 다이얼로그 생성 기능 구현

--- app/src/main/java/com/puzzling/aoslaboratory/presentation/main/MainActivity.kt
+++ app/src/main/java/com/puzzling/aoslaboratory/presentation/main/MainActivity.kt
@@ -4,10 +4,18 @@ import android.os.Bundle
 import com.puzzling.aoslaboratory.R
 import com.puzzling.aoslaboratory.base.BaseActivity
 import com.puzzling.aoslaboratory.databinding.ActivityMainBinding
+import com.puzzling.aoslaboratory.presentation.register.CodeCopyDialogFragment
 
 class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+    private val codeCopyDialog: CodeCopyDialogFragment by lazy { CodeCopyDialogFragment() }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        clickRegisterBtn()
+    }
+
+    private fun clickRegisterBtn() {
+        binding.btnHomeRegister.setOnClickListener {
+            codeCopyDialog.show(supportFragmentManager, "LOADING_DIALOG")
+        }
     }
 }
--- app/src/main/java/com/puzzling/aoslaboratory/presentation/register/CodeCopyDialogFragment.kt
+++ app/src/main/java/com/puzzling/aoslaboratory/presentation/register/CodeCopyDialogFragment.kt
@@ -1,8 +1,36 @@
 package com.puzzling.aoslaboratory.presentation.register
 
-import com.puzzling.aoslaboratory.R
-import com.puzzling.aoslaboratory.base.BaseFragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
 import com.puzzling.aoslaboratory.databinding.FragmentCodeCopyDialogBinding
 
 class CodeCopyDialogFragment :
-    BaseFragment<FragmentCodeCopyDialogBinding>(R.layout.fragment_code_copy_dialog)
+    DialogFragment() {
+    private var _binding: FragmentCodeCopyDialogBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentCodeCopyDialogBinding.inflate(inflater, container, false)
+        val view = binding.root
+        clickCloseBtn()
+        return view
+    }
+
+    private fun clickCloseBtn() {
+        binding.btnDialogClose.setOnClickListener {
+            dialog?.dismiss()
+        }
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}
