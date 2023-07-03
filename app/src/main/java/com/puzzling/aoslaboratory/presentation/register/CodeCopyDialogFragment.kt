d1a13182f238d7fa5c469e8bed4e4de1fe0d2a3d
Cat-JiYoung <kimyamae82@gmail.com>
2023-07-03 18:34:58 +0900

    [FEAT] 참여 코드 생성 다이얼로그 뷰 xml 구현

---
+++ app/src/main/java/com/puzzling/aoslaboratory/presentation/register/CodeCopyDialogFragment.kt
@@ -0,0 +1,8 @@
+package com.puzzling.aoslaboratory.presentation.register
+
+import com.puzzling.aoslaboratory.R
+import com.puzzling.aoslaboratory.base.BaseFragment
+import com.puzzling.aoslaboratory.databinding.FragmentCodeCopyDialogBinding
+
+class CodeCopyDialogFragment :
+    BaseFragment<FragmentCodeCopyDialogBinding>(R.layout.fragment_code_copy_dialog)
