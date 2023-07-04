466600cbb1510692fb3e2459ad6e55d515d65d82
emjayMJkim <minjeong030420@gmail.com>
2023-07-05 08:59:17 +0900

    [FEAT] datePickerDialog 구현

---
+++ app/src/main/java/com/puzzling/aoslaboratory/presentation/register/RegisterActivity.kt
@@ -0,0 +1,44 @@
+package com.puzzling.aoslaboratory.presentation.register
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import com.puzzling.aoslaboratory.R
+import com.puzzling.aoslaboratory.base.BaseActivity
+import com.puzzling.aoslaboratory.databinding.ActivityRegisterBinding
+import com.puzzling.aoslaboratory.util.ViewModelFactory
+import java.text.SimpleDateFormat
+import java.util.*
+
+class RegisterActivity :
+    BaseActivity<ActivityRegisterBinding>(R.layout.activity_register) {
+    private val viewModel: RegisterViewModel by viewModels { ViewModelFactory(this) }
+
+    private val calendar = Calendar.getInstance()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding.viewModel = viewModel
+
+        clickDatePicker()
+    }
+
+    private fun clickDatePicker() {
+        binding.btnDateDropDown.setOnClickListener {
+            pickedDate()
+        }
+    }
+
+    private fun pickedDate() {
+        val bottomSheetFragment = DatePickerFragment()
+        bottomSheetFragment.setOnDateSelectedListener(object : DatePickerFragment.OnDateSelectedListener {
+            override fun onDateSelected(year: Int, month: Int, dayOfMonth: Int) {
+                val selectedDate = Calendar.getInstance()
+                selectedDate.set(year, month, dayOfMonth)
+                val formattedDate = SimpleDateFormat("yyyy/MM/dd", Locale.getDefault()).format(selectedDate.time)
+                binding.tvDateDropDown.text = formattedDate
+            }
+        })
+
+        bottomSheetFragment.show(supportFragmentManager, bottomSheetFragment.tag)
+    }
+}
